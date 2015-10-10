e957d8cc0ea0c61fdd4376cedc12608805eb8d6d
Aman <aaggarwal73@gatech.edu>
2015-10-10 16:26:36 -0400

    FIRST WORKING DOC

--- app/src/main/java/squadup/localslang/MainActivity.java
+++ app/src/main/java/squadup/localslang/MainActivity.java
@@ -1,37 +1,69 @@
 package squadup.localslang;
 
-import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+
+import squadup.localslang.R;
+import squadup.localslang.expandListView.Adapter.ExpandListAdapter;
+import squadup.localslang.expandListView.Classes.*;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<ExpandListGroup> ExpListItems;
+    private ExpandableListView ExpandList;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ExpandList = (ExpandableListView) findViewById(R.id.ExpList);
+        ExpListItems = SetStandardGroups();
+        ExpAdapter = new ExpandListAdapter(MainActivity.this, ExpListItems);
+        ExpandList.setAdapter(ExpAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public ArrayList<ExpandListGroup> SetStandardGroups() {
+        ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
+        ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
+        ExpandListGroup gru1 = new ExpandListGroup();
+        gru1.setName("Hip");
+        ExpandListChild ch1_1 = new ExpandListChild();
+        ch1_1.setName("A movie");
+        ch1_1.setTag(null);
+        list2.add(ch1_1);
+        ExpandListChild ch1_2 = new ExpandListChild();
+        ch1_2.setName("An other movie");
+        ch1_2.setTag(null);
+        list2.add(ch1_2);
+        ExpandListChild ch1_3 = new ExpandListChild();
+        ch1_3.setName("And an other movie");
+        ch1_3.setTag(null);
+        list2.add(ch1_3);
+        gru1.setItems(list2);
+        list2 = new ArrayList<ExpandListChild>();
+
+        ExpandListGroup gru2 = new ExpandListGroup();
+        gru2.setName("Action");
+        ExpandListChild ch2_1 = new ExpandListChild();
+        ch2_1.setName("A movie");
+        ch2_1.setTag(null);
+        list2.add(ch2_1);
+        ExpandListChild ch2_2 = new ExpandListChild();
+        ch2_2.setName("An other movie");
+        ch2_2.setTag(null);
+        list2.add(ch2_2);
+        ExpandListChild ch2_3 = new ExpandListChild();
+        ch2_3.setName("And an other movie");
+        ch2_3.setTag(null);
+        list2.add(ch2_3);
+        gru2.setItems(list2);
+        list.add(gru1);
+        list.add(gru2);
+
+        return list;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
