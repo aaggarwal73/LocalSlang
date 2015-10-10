146c1ba37a3be01a518b9cc3a598f6c752c7616b
Aman <aaggarwal73@gatech.edu>
2015-10-10 17:17:45 -0400

    We got some kind of action!

--- app/src/main/java/squadup/localslang/MainActivity.java
+++ app/src/main/java/squadup/localslang/MainActivity.java
@@ -1,7 +1,8 @@
 package squadup.localslang;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
+import java.io.File;
 import squadup.localslang.R;
 import squadup.localslang.expandListView.Adapter.ExpandListAdapter;
 import squadup.localslang.expandListView.Classes.*;
@@ -28,40 +29,51 @@ public class MainActivity extends Activity {
     public ArrayList<ExpandListGroup> SetStandardGroups() {
         ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
         ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
+        //Scanner scan = new Scanner(new File(HoCo.txt));
+        //for(String s: scan.nextLine() ){ }
+
         ExpandListGroup gru1 = new ExpandListGroup();
         gru1.setName("Hip");
         ExpandListChild ch1_1 = new ExpandListChild();
-        ch1_1.setName("A movie");
+        ch1_1.setName("Aware of, knowledgeable about");
         ch1_1.setTag(null);
         list2.add(ch1_1);
-        ExpandListChild ch1_2 = new ExpandListChild();
-        ch1_2.setName("An other movie");
-        ch1_2.setTag(null);
-        list2.add(ch1_2);
-        ExpandListChild ch1_3 = new ExpandListChild();
-        ch1_3.setName("And an other movie");
-        ch1_3.setTag(null);
-        list2.add(ch1_3);
         gru1.setItems(list2);
         list2 = new ArrayList<ExpandListChild>();
 
         ExpandListGroup gru2 = new ExpandListGroup();
-        gru2.setName("Action");
+        gru2.setName("Cuffed");
         ExpandListChild ch2_1 = new ExpandListChild();
-        ch2_1.setName("A movie");
+        ch2_1.setName("To be in a committed relationship");
         ch2_1.setTag(null);
         list2.add(ch2_1);
-        ExpandListChild ch2_2 = new ExpandListChild();
-        ch2_2.setName("An other movie");
-        ch2_2.setTag(null);
-        list2.add(ch2_2);
-        ExpandListChild ch2_3 = new ExpandListChild();
-        ch2_3.setName("And an other movie");
-        ch2_3.setTag(null);
-        list2.add(ch2_3);
         gru2.setItems(list2);
+        list2 = new ArrayList<ExpandListChild>();
+
+        ExpandListGroup gru3 = new ExpandListGroup();
+        gru3.setName("HoCo");
+        ExpandListChild ch3_1 = new ExpandListChild();
+        ch3_1.setName("Howard County, Maryland");
+        ch3_1.setTag(null);
+        list2.add(ch3_1);
+        gru3.setItems(list2);
+        list2 = new ArrayList<ExpandListChild>();
+
+        ExpandListGroup gru4 = new ExpandListGroup();
+        gru4.setName("MoCo");
+        ExpandListChild ch4_1 = new ExpandListChild();
+        ch4_1.setName("Montgomery County, Maryland");
+        ch4_1.setTag(null);
+        list2.add(ch4_1);
+        gru4.setItems(list2);
+        list2 = new ArrayList<ExpandListChild>();
+
         list.add(gru1);
         list.add(gru2);
+        list.add(gru3);
+        list.add(gru4);
+
+
 
         return list;
     }
